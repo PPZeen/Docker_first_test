46d2020b5fa2b208cb4769d5a04c088bf97d2c03
Zenzor <P.nutthavat_@hotmail.com>
2024-01-07 16:19:52 +0700

    initial commit

---
+++ api-dev/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const mysql = require('mysql2/promise')
+
+const app = express()
+const port = 8080
+
+app.get("/", (req, res) => {
+    res.send("Index api-dev")
+})
+
+app.get("/users", async (req, res) => {
+    try {
+        const connection = await connectionMySQL()
+        const [results] = await connection.query('SELECT * FROM `users`')
+        
+        res.json(results)
+    } catch (err) {
+        res.send("please, reload decker....")
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http:///localhost:${port}.`)
+})
+
+const connectionMySQL = async () => {
+    try {
+        const connection = await mysql.createConnection({
+            host: 'db-dev',
+            user: 'root',
+            password: 'root',
+            database: 'db-dev'
+        })
+        return connection
+    } catch (err) {
+        return "Connection failed."
+    }
+}
